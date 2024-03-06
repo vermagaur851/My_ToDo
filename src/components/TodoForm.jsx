793f5345d2f5ebf2f99042739869ba8525565570
Gaurav Kumar <138775489+vermagaur851@users.noreply.github.com>
2024-03-06 15:48:40 +0530

    Update TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -8,7 +8,7 @@ function TodoForm() {
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addToDo(input));
+    if(e.target.value!='') dispatch(addToDo(input));
     setInput('');
   };
 
